266496e7c3c6160811106e71ab5b46eba16ebbe6
Germo Aabla <germo.aabla.it@gmail.com>
2024-12-09 21:56:46 +0200

    Week7Functions

---
+++ CalculatorWithFunctions/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 5;
+int b = 6;
+
+Add(a, b);
+Add(10, 100);
+
+static void Add(int x, int y)
+{
+    Console.WriteLine($"{x} + {y} = {x + y}");
+}
+
+Multiply(a, b);
+
+static void Multiply(int x, int y)
+{
+    Console.WriteLine($"{x} * {y} = {x * y}");
+
+}
+
+Subtract(a, b);
+
+static void Subtract(int x, int y)
+{
+    Console.WriteLine($"{x} - {y} = {x - y}");
+}
---
+++ DisplayArrayElementsFunction/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] fruitBasket = { "apples", "bananas", "oranges" };
+string[] pets = { "cats", "dogs", "fish" };
+string[] characters = { "Harry", "Ron" };
+int[] numbers = { 1, 2, 3, 4, 5, 6 };
+
+PrintArrayElements(fruitBasket);
+PrintArrayElements(pets);
+PrintArrayElements(characters);
+//PrintArrayElements(numbers); - problem with data type!
+
+static void PrintArrayElements(string[] SomeArray)
+{
+    for (int i = 0; i < SomeArray.Length; i++)
+    {
+        Console.WriteLine($"{i+1}. {SomeArray[i]}");
+    }
+}
